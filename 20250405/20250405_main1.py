ec6a016fe8136877a4554f7cef7e44f9b8fe65d9
devforyou0523 <devforyou0523@gmail.com>
2025-04-06 22:59:19 +0900

    add 1008, add 10869

---
+++ 20250405/20250405_main1.py
@@ -0,0 +1,6 @@
+#1008번
+input_number = input()
+a = float(input_number.split(" ")[0])
+b = float(input_number.split(" ")[1])
+
+print("{}".format(a/b))
---
+++ 20250406/20250406_main1.py
@@ -0,0 +1,10 @@
+#10869번 문제
+input_number = input()
+a = int(input_number.split(" ")[0])
+b = int(input_number.split(" ")[1])
+
+print("{}".format(a+b))
+print("{}".format(a-b))
+print("{}".format(a*b))
+print("{}".format(int(a/b)))
+print("{}".format(a%b))

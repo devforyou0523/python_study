c8db8bc6fbc784d8c01658ce65e0ba7513d1c07a
devforyou0523 <devforyou0523@gmail.com>
2025-04-08 14:04:51 +0900

    edit 2581 ver1

--- 20250408/20250408_main3.py
+++ 20250408/20250408_main3.py
@@ -3,17 +3,20 @@ first_input_number = int(input())
 second_input_number = int(input())
 prime_number_list = []
 
-if first_input_number == 1 & second_input_number == 1:
-    print(-1)
-for number in range(first_input_number, second_input_number +1):
-    for i in range (2, number):
-        if number % i == 0:
-            break
-        elif i+1 == number:
-            prime_number_list.append(number)
-            break
-if len(prime_number_list) == 0:
+if first_input_number ==  second_input_number:
     print(-1)
 else:
-    print(sum(prime_number_list))
-    print(min(prime_number_list))
+    if first_input_number == 0:
+        prime_number_list.append(1)
+    for number in range(first_input_number, second_input_number +1):
+        for i in range (2, number):
+            if number % i == 0:
+                break
+            elif i+1 == number:
+                prime_number_list.append(number)
+                break
+    if len(prime_number_list) == 0:
+        print(-1)
+    else:
+        print(sum(prime_number_list))
+        print(min(prime_number_list))
--- 20250408/tempCodeRunnerFile.py
+++ 20250408/tempCodeRunnerFile.py
@@ -1,12 +1,19 @@
-for number in range(first_input_number, second_input_number +1):
-    for i in range (2, number):
-        if number % i == 0:
-            break
-        elif i == number:
-            prime_number_list.append(number)
-            break
-if len(prime_number_list) == 0:
+first_input_number = int(input())
+second_input_number = int(input())
+prime_number_list = []
+
+if first_input_number ==  second_input_number:
     print(-1)
 else:
-    print(sum(prime_number_list))
-    print(min(prime_number_list))
+    for number in range(first_input_number, second_input_number +1):
+        for i in range (2, number):
+            if number % i == 0:
+                break
+            elif i+1 == number:
+                prime_number_list.append(number)
+                break
+    if len(prime_number_list) == 0:
+        print(-1)
+    else:
+        print(sum(prime_number_list))
+        print(min(prime_number_list))

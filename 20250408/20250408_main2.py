edae0230498562bdb46cd2c8bd762c130cce8b47
devforyou0523 <devforyou0523@gmail.com>
2025-04-08 13:25:06 +0900

    add 2588

---
+++ 20250408/20250408_main2.py
@@ -0,0 +1,8 @@
+#2588번
+input_first_number = input()
+input_second_number = input()
+
+print("{}".format(int(input_first_number) * int(input_second_number[2])))
+print("{}".format(int(input_first_number) * int(input_second_number[1])))
+print("{}".format(int(input_first_number) * int(input_second_number[0])))
+print("{}".format(int(input_first_number) * int(input_second_number)))

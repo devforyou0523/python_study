170f03bbdf9c02257a514b831c5bd6d3bb25a6e2
devforyou0523 <devforyou0523@gmail.com>
2025-04-08 13:48:01 +0900

    add 2581, todo 2581

--- 20250408/tempCodeRunnerFile.py
+++ 20250408/tempCodeRunnerFile.py
@@ -1,15 +1,8 @@
-#2581번(수정필요)
-first_input_number = int(input())
-second_input_number = int(input())
-prime_number_list = []
-
-if first_input_number == 1 & second_input_number == 1:
-    print(-1)
 for number in range(first_input_number, second_input_number +1):
     for i in range (2, number):
         if number % i == 0:
             break
-        elif i+1 == number:
+        elif i == number:
             prime_number_list.append(number)
             break
 if len(prime_number_list) == 0:

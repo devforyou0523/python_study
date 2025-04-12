2b44e81aac3ed95b6c325d9ce2076362484c97c9
devforyou0523 <devforyou0523@gmail.com>
2025-04-12 15:15:39 +0900

    add 9498

---
+++ 20250412/20250412_main2.py
@@ -0,0 +1,13 @@
+#9498번
+input_number = int(input())
+
+if (90<=input_number<=100):
+    print("A")
+elif (80<=input_number<=89):
+    print("B")
+elif (70<=input_number<=79):
+    print("C")
+elif (60<=input_number<=69):
+    print("D")
+else:
+    print("F")

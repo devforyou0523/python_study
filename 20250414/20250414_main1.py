d66e51160426d5fd5348481a86857d2b45aef4e6
devforyou0523 <devforyou0523@gmail.com>
2025-04-15 13:04:02 +0900

    edit 2753 in 20250414

--- 20250414/20250414_main1.py
+++ 20250414/20250414_main1.py
@@ -1,10 +1,6 @@
 #2753번 (수정필요)
-input_number = float(input())
-
-print(f"input_number/4 is {input_number / 4}")
-if (input_number % 4 == 0 and input_number%100 != 0.0):
+input_number = int(input())
+if ((input_number % 4 == 0 and input_number%100 != 0) or (input_number % 400 == 0)):
     print ("1")
-elif (input_number%400 == 0.0):
-    print("1")
 else:
     print("0")

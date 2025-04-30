d78a1f7de5fc8289a5cce83c1362dfe3d487041c
devforyou0523 <devforyou0523@gmail.com>
2025-04-30 14:50:27 +0900

    todo 10813

---
+++ 20250430/20250430_main1.py
@@ -0,0 +1,12 @@
+#10813번
+basket_number, input_number = map(int, input().split(" "))
+last_basket = [i for i in range(1, basket_number+1) ]
+
+for i in range (input_number):
+    start_basket, end_basket, add_ball = map(int, input().split(" "))
+    temp_basket = 0
+    last_basket[start_basket-1] = temp_basket
+    last_basket[end_basket-1] = last_basket[start_basket-1]
+    last_basket[start_basket-1] = temp_basket
+
+print(str(last_basket)[1:-1].replace(",",""))
